5e02d8cd8fd0b68f12a74fcba4d3314331d2ba42
Javier Zarate <zarate.javier.jz@gmail.com>
2020-07-15 10:55:56 -0600

    added tab navigation

--- App.js
+++ App.js
@@ -1,10 +1,39 @@
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import StackDemo from './components/StackDemo';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {View, Text} from "react-native"
+
+
+function SettingsScreen({navigation}) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>settings Screen</Text>
+      
+    </View>
+  );
+}
+
+function UsersScreen({navigation}) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Users Screen</Text>
+      
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator()
+
 function App() {
   return (
     <NavigationContainer>
-      <StackDemo />
+      {/* <StackDemo /> */}
+      <Tab.Navigator>
+        <Tab.Screen name={"Users"} component={UsersScreen} />
+        <Tab.Screen name={"Settings"} component={StackDemo}/>
+        
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
