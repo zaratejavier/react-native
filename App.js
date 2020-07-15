a82629ca0f58c1555734ce96fabf33ac088696c7
Javier Zarate <zarate.javier.jz@gmail.com>
2020-07-15 10:27:18 -0600

    ADDED PROPS SCREEN

--- App.js
+++ App.js
@@ -14,10 +14,33 @@ function HomeScreen({navigation}) {
   );
 }
 
-function optionsScreen() {
+function OptionsScreen({navigation}) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>options Screen</Text>
+       <Button title="Go to  Props screen"
+        onPress={() => navigation.navigate('Props', {
+          userName: "Timmothy",
+        })}
+      />
+      <Button title="Go back"
+        onPress={() => navigation.goback()}
+      />
+    </View>
+  );
+}
+
+
+function PropsScreen({ navigation, route, myProp }) {
+  const {userName} = route.params
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Props Screen</Text>
+      <Text>{userName}</Text>
+      <Text>{myProp}</Text>
+       <Button title="Go to  home"
+        onPress={() => navigation.navigate('Home')}
+      />
     </View>
   );
 }
@@ -29,7 +52,18 @@ function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Options" component={optionsScreen} />
+        <Stack.Screen name="Options" component={OptionsScreen} options={{
+          title: "My Options",
+          headerStyle: {
+            backgroundColor: '#456789',
+          },
+          headerTintColor: '#fff'
+        }} />
+
+        <Stack.Screen name="Props" >
+          {(props) => <PropsScreen {...props} myProp={"yoyo"}/>}
+        </Stack.Screen>
+
 
       </Stack.Navigator>
     </NavigationContainer>
